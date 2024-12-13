3758b533b597c73a665b6055d5971f02d7e7c583
Jhonjaxde <jjarteaga@uce.edu.ec>
2024-12-13 01:46:49 +0000

    tarea07

--- js/formulario.js
+++ js/formulario.js
@@ -4,49 +4,24 @@ const datos=[
 const app = Vue.createApp({
     
     methods: {
-       agregarFrase() {
-        console.log(this.nombre);
-        console.log(this.apellido);
-        console.log(this.hobby);
-        const nuevaFrase={
+      
+       
+        guardarDatosFinal() {
+        
+        const nuevosDatos={
             nombre: this.nombre,
             apellido: this.apellido,
-            hobby: this.hobby
+            hobby: this.hobby,
+            fechaNac: this.fechaNac
         }
 
-        this.listadatos.unshift(nuevaFrase);
-       },
-       agregarFraseFinal() {
-        console.log(this.nombre);
-        console.log(this.apellido);
-        console.log(this.hobby);
-        const nuevaFrase={
-            nombre: this.nombre,
-            apellido: this.apellido,
-            hobby: this.hobby
-        }
-
-        this.listadatos.push(nuevaFrase);
-       },
-       eventoKeyPress({chartCode,cancelable,key,keyCode,target}){
-        if(key==="Enter"){
-        console.log('Evento');
-        //console.log(event);
-        console.log(chartCode);
-        console.log(cancelable);
-        console.log(key);
-        console.log(keyCode);
-        console.log(target.baseURI);
-        this.agregarFraseFinal();
-        }
-       },
-       eventoKeyPressModificador(){
-        
-        console.log('Evento');
-        //console.log(event);
-        this.agregarFraseFinal();
-        
+        this.listadatos.push(nuevosDatos);
+        this.nombre = null;
+        this.apellido = null;
+        this.hobby = null;
+        this.fechaNac = null;
        }
+       
     },
     data(){
         return{
@@ -54,10 +29,11 @@ const app = Vue.createApp({
           nombre:null,
           //autor:'sin autor'
           apellido:null,
-          hobby:null
+          hobby:null,
+          fechaNac:null
         }
     }
 })
  
-app.mount('#myFormulario')
+app.mount('#idMyFormulario')
 
